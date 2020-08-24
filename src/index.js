3e8152617dce35efbd1aec5403819aaf40d40aa7
Erickandela <erickandela_rocker@hotmail.com>
2020-08-24 12:47:53 -0500

    lifecycle components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { render } from 'react-dom';
 
 const Button = () =>{
   const[counter, setCounter] = useState(0);
-  return (
-    <div>
-    <p> Presionado: { counter } </p>
-    <button onClick={ ()=> setCounter(counter + 1)}> Haz Click</button>
-    </div>
-  )
-    
-  
+
+  useEffect (()=>{
+    console.log("Me ejecuté");
+    return ()=>{
+      console.log("Adios");
+    }
+  },[]);
+  return <button onClick={ ()=> setCounter(counter + 1)}> Haz Click {counter}</button>
 }
 
 const App = () =>{
