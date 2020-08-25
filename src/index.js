d03e07caa3ff24c81a64636f9ed1be2dbce2c2c5
Erickandela <erickandela_rocker@hotmail.com>
2020-08-25 10:25:28 -0500

    Forms

--- src/index.js
+++ src/index.js
@@ -1,18 +1,46 @@
 import React, {useState} from 'react';
 import { render } from 'react-dom';
 
-const Saludo = ()=>{
-  const [name, setName] = useState("");
-  return(
+const Form = ()=>{
+  let [title, setTitle] = useState("");
+  let [body, setBody] = useState("");
+
+  const sendForm = (ev)=>{
+    ev.preventDefault()
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: title,
+      body: body,
+      userId: 1
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then(response => response.json())
+  .then(json => {
+    setTitle("");
+    setBody("");
+    console.log(json)})
+  }
+  return (
+    <form onSubmit = { (ev)=> sendForm(ev)}>
     <div>
-    <input type="text" onChange={ (ev)=> setName(ev.target.value)} />
-    <p> Hola {name} </p>
+    <label htmlfor="title">Titulo</label>
+    <input type="text" value={title} id="title" onChange={(ev)=> setTitle(ev.target.value)} />
     </div>
-  );
+    <div>
+    <label htmlfor="body">Publicación</label>
+    <textarea id="body" value={body} onChange={(ev)=> setBody(ev.target.value)}></textarea>
+    </div>
+    <input type="submit" value="Enviar"/>
+    </form>
+  )
 }
 
 const App = () =>{
-  return <div> <Saludo /></div>;
+  return <div> <Form /></div>;
 }
 
 render(<App />, document.getElementById('root'));
