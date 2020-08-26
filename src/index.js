cae24d97763c54a07ef5959a7a65676cd0338936
Erickandela <erickandela_rocker@hotmail.com>
2020-08-26 17:23:55 -0500

    State_contador

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { render } from 'react-dom';
+import PropTypes from 'prop-types';
 
 class App extends React.Component{
+  static defaultProps={
+    name: "erick"
+  }
+
+  static propTypes = {
+    name: PropTypes.string
+  }
+
+  constructor(props){
+    super(props);
+    this.state = {
+      contador: 0
+    }
+  }
+
   render(){
-    return <p> Hola Mundo </p>
+    console.log(this.props);
+    return (
+      <>
+    <p> Contador: { this.state.contador}</p>
+    <button onClick={ ()=> this.setState({contador: this.state.contador + 1}) }>Sumar</button>
+    </>
+    )
+    
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App name={2}/>, document.getElementById('root'));
